033210edf283833de514847ccb2b858c41f8618a
Blasty1 <ichigoplayer18@gmail.com>
2020-11-21 12:50:28 +0100

    ok

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-I'm learning vue.js
+-   DIRETTIVE   -
+v-if --> if di qualsiasi altro linguaggio
+v-for --> for di qualsiasi altro linguaggio
+v-bind:key:'varibale.id' --> da utilizzare con un for
+v-on:event-name='namefunctiontocall' -> quando fai un determinato evento si attiva la funzione contenuta nei methods del oggetto vue
+Vue.component(
+    'box' , {
+        props : ['article'],
+        template : '<div> <div class="title"> <h1> {{ article.name }} </h1> </div> <div class="corpo"> <p> {{ article.body }}</p></div> </div> '
+    }
+) ---> how to create a component
+v-bind:article='name variable'
+
+-   CREATE VUEAPP   -
+const app = Vue.createApp ( {
+    data(){
+        return {
+            variable : value
+        }
+    }
+    methods : {
+        method1 : function(){},
+    }
+})
+app.mount('#nameElement');
--- home.js
+++ home.js
@@ -1,60 +1,21 @@
-Vue.component(
-    'box' , {
-        props : ['article'],
-        template : '<div> <div class="title"> <h1> {{ article.name }} </h1> </div> <div class="corpo"> <p> {{ article.body }}</p></div> </div> '
-    }
-)
-let dataToShow = [
-    { id : 0 , name : 'bryan si fa le pip' , body : 'sldasklksajasdasdkksdakasjkdlaskjdk'},
-    { id : 1 , name : 'angelo è gay' , body : 'sajkjhkdqwjo asdfjjkfahfhasdfasd asdfjkds'},
-    { id : 2 , name : 'l\' ultimo testo ' , body : 'ok bdasdhsahdhwhaofdiasjhf'}
-]
-let app = new Vue(
+
+
+
+let app2 = Vue.createApp(
     {
-        el : '.app',
-        data : {
-            messageToShow : 'prima scritta in vue' 
+         data() {
+            return {
+                        logging : 10 , 
+                        test : 'ok',
+                        dataToShow : [
+                            { id : 0 , name : 'bryan si fa le pip' , body : 'sldasklksajasdasdkksdakasjkdlaskjdk'},
+                            { id : 1 , name : 'angelo è gay' , body : 'sajkjhkdqwjo asdfjjkfahfhasdfasd asdfjkds'},
+                            { id : 2 , name : 'l ultimo testo ' , body : 'ok bdasdhsahdhwhaofdiasjhf'}
+                        ]
+                    } 
         }
     }
 )
 
-let app2 = new Vue(
-    {
-        el : '#ok',
-        data : {
-            logging : 10 , 
-            test : 'ok',
-            dataToShow : [
-                { id : 0 , name : 'bryan si fa le pip' , body : 'sldasklksajasdasdkksdakasjkdlaskjdk'},
-                { id : 1 , name : 'angelo è gay' , body : 'sajkjhkdqwjo asdfjjkfahfhasdfasd asdfjkds'},
-                { id : 2 , name : 'l ultimo testo ' , body : 'ok bdasdhsahdhwhaofdiasjhf'}
-            ]
-        }
-    }
-)
 
-let listaApp = new Vue(
-    {
-        el : '.listaLink',
-        data : {
-            links : [
-                { name : 'bryan'},
-                { name : 'giovanni'},
-                { name : 'sergio'},
-                { giovanni : 'burno'}
-
-            ],
-            buttonText : 'Schiacciamo e scoprirai il tuo nome'
-        },
-        methods : {
-            changeColor : function (event){
-                console.log('ok')
-                event.target.style.backgroundColor = 'red'
-            },
-            showName : function(){
-                this.buttonText = this.links[1].name
-            }
-        }
-    }
-)
 
